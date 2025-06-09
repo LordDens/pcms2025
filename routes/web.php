b8610c2781d84c08a30d1faf410f803689200f2e
LordDens <movabitpls@gmail.com>
2025-06-09 21:06:18 +0700

    feat: tambah middleware CheckAge untuk validasi usia pendaftaran KTP

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/pendaftaran-ktp', function () {
+    return 'Selamat datang di halaman Pendaftaran KTP Online';
+})->middleware('check.age');
+
 use App\Http\Controllers\RentController;
 
 Route::get('/', function () {
     return view('home');
 });
+
+
 
 Route::resource('rents', RentController::class);
 
